bcff93db0fa3fc3c852f740e8ff14d01341499bc
gialnet <antonio.gialnet@gmail.com>
2023-08-15 19:20:32 +0200

    save several projects

---
+++ elasticsearch/elastic.sh
@@ -0,0 +1,5 @@
+export ES_HOME=/usr/local/elastic/elasticsearch-7.14.1
+export ES_PATH_CONF=${ES_HOME}/config
+export ES_PATH_LOGS=${ES_HOME}/logs
+export ES_PATH_PLUGINS=${ES_HOME}/plugins
+export ES_PATH_TEMP=${ES_HOME}/temp
---
+++ microk8s/notas.md
@@ -0,0 +1,3 @@
+
+ansible-playbook -i inventory microk8s.yaml -u antonio -K
+
